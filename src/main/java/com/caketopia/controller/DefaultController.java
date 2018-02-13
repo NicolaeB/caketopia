db455f60e3ac2be0ed7ff92a9d1d37525fd780a5
Nicolae <nicu.besliu@gmail.com>
2018-02-13 19:01:28 +0200

    one-page-wonder styles

--- src/main/java/com/caketopia/controller/DefaultController.java
+++ src/main/java/com/caketopia/controller/DefaultController.java
@@ -6,35 +6,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("/")
-    public String home1() {
-        return "/home";
-    }
+	@GetMapping("/")
+	public String landing() {
+		return "/home";
+	}
 
     @GetMapping("/home")
     public String home() {
         return "/home";
     }
 
+	@GetMapping("/gallery")
+	public String gallery() {
+		return "/gallery";
+	}
+
     @GetMapping("/admin")
     public String admin() {
         return "/admin";
     }
 
-    @GetMapping("/user")
-    public String user() {
-        return "/user";
+    @GetMapping("/blog")
+    public String blog() {
+        return "/blog";
     }
-
-    @GetMapping("/about")
-	public String about() {
-		return "/about";
-	}
 
 	@GetMapping("/menu")
 	public String menu() {
 		return "/menu";
 	}
+	@GetMapping("/contact")
+	public String contact() {
+		return "/contact";
+	}
+
 
     @GetMapping("/login")
     public String login() {
